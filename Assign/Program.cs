91ba7e63f4ac51027b654cbe6553a7da513a06e8
Bob Cohen <bobcohen1969@yahoo.com>
2017-10-28 12:26:07 -0500

    Arithmetic Examples

--- Assign/Program.cs
+++ Assign/Program.cs
@@ -10,6 +10,33 @@ namespace Assign
     {
         static void Main(string[] args)
         {
+            Console.Title = "Assign";
+
+            int a;
+            int b;
+
+            Console.Write("Assign Values: ");
+            Console.Write("\ta = " + (a = 8));
+            Console.Write("\tb = " + (b = 4));
+            
+            Console.Write("\n\nAdd and Assign: ");
+            Console.Write("\t a += b (8 += 4)\t a = " + (a += b));
+
+            Console.Write("\n\nSubtract and Assign: ");
+            Console.Write("\t a -= b (12 -= 4)\t a = " + (a -= b));
+
+            Console.Write("\n\nMuliply and Assign: ");
+            Console.Write("\t a *= b (8 *= 4)\t a = " + (a *= b));
+
+            Console.Write("\n\nDivide and Assign: ");
+            Console.Write("\t a /= b (32 /= 4)\t a = " + (a /= b));
+
+            Console.Write("\n\nModulus and Assign: ");
+            Console.Write("\t a %= b (8 %= 4)\t a = " + (a %= b));
+
+            Console.ReadKey();
+                
+                
         }
     }
 }
