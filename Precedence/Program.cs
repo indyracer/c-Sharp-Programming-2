e4a4cbc7308446246a3b523658b1f4e1ccd88b0a
Bob Cohen <bobcohen1969@yahoo.com>
2017-11-03 20:39:54 -0500

    Precedence

--- Precedence/Program.cs
+++ Precedence/Program.cs
@@ -10,6 +10,23 @@ namespace Precedence
     {
         static void Main(string[] args)
         {
+            Console.Title = "Precedence";
+
+            int sum;
+
+            sum = 1 + 4 * 3;
+            Console.WriteLine("Default Order:\t\t" + sum);
+
+            sum = (1 + 4) * 3;
+            Console.WriteLine("Forced Order:\t\t" + sum);
+
+            sum = 7 - 4 + 2;
+            Console.WriteLine("\nDefault Direction:\t " + sum);
+
+            sum = 7 - (4 + 2);
+            Console.WriteLine("Forced Direction:\t" + sum);
+
+            Console.ReadKey();
         }
     }
 }
