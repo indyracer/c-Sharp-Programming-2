4ed3216c312d0891ede6a0d5f47ff1177ef32bf3
Bob Cohen <bobcohen1969@yahoo.com>
2017-11-03 20:29:49 -0500

    Condition

--- Condition/Program.cs
+++ Condition/Program.cs
@@ -10,6 +10,22 @@ namespace Condition
     {
         static void Main(string[] args)
         {
+            Console.Title = "Condition";
+
+            int a = 8, b = 3;
+
+            String verb = (a != 1) ? " are " : " is ";
+            Console.Write("There" + verb + a + "\n");
+
+            string parity = (a % 2 != 0) ? "Odd" : "Even";
+            Console.WriteLine(a + " is " + parity);
+
+            parity = (b % 2 != 0) ? "Odd" : "Even";
+            Console.WriteLine(b + " is " + parity);
+
+            int max = (a > b) ? a : b;
+            Console.Write("\nMaximum is " + max);
+            Console.ReadKey();
         }
     }
 }
