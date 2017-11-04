c9174b114a6576e55c7c50818f19d95df9544794
Bob Cohen <bobcohen1969@yahoo.com>
2017-11-04 12:46:47 -0500

    Switch statement

--- Switch/Program.cs
+++ Switch/Program.cs
@@ -10,6 +10,42 @@ namespace Switch
     {
         static void Main(string[] args)
         {
+            Console.Title = "Switch";
+
+            Console.Write("Please enter a number between 1 and 7:  ");
+            int num = Convert.ToInt16(Console.ReadLine());
+
+            string day;
+
+            switch (num)
+            {
+                case 1:
+                    day = "Monday";
+                    break;
+                case 2:
+                    day = "Tuesday";
+                    break;
+                case 3:
+                    day = "Wednesday";
+                    break;
+                case 4:
+                    day = "Thursday";
+                    break;
+                case 5:
+                    day = "Friday";
+                    break;
+                case 6:
+                    day = "Saturday";
+                    break;
+                case 7:
+                    day = "Sunday";
+                    break;
+                default:
+                    day = "No day like today";
+                    break;
+            }
+            Console.WriteLine("Day " + num + ": " + day);
+            Console.ReadKey();
         }
     }
 }
