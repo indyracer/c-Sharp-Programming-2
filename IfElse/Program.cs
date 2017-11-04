d799dd02c89b4941a136a67b1c2a97c940bcdd2e
Bob Cohen <bobcohen1969@yahoo.com>
2017-11-04 12:30:35 -0500

    If Else statements

--- IfElse/Program.cs
+++ IfElse/Program.cs
@@ -10,6 +10,29 @@ namespace IfElse
     {
         static void Main(string[] args)
         {
+            Console.Title = "IfElse";
+
+            Console.Write("Enter a number: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Thanks. Now enter a letter: ");
+            char letter = Convert.ToChar(Console.ReadLine());
+
+            //output message if number input exceeds specified value
+            if(num >= 6)
+            {
+                Console.WriteLine("\nNumber exceeds 5");
+                //Nested statement to check if letter is specified value
+                if(letter == 'C')
+                {
+                    Console.WriteLine("Letter is 'C'");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nNumber is 5 or less");
+            }
+
+            Console.ReadKey();
         }
     }
 }
