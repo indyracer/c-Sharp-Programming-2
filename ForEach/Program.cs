c003938b3210a4c0349ec7e35926cf8635da7ba3
Bob Cohen <bobcohen1969@yahoo.com>
2017-11-05 14:19:07 -0600

    ForEach

--- ForEach/Program.cs
+++ ForEach/Program.cs
@@ -10,6 +10,20 @@ namespace ForEach
     {
         static void Main(string[] args)
         {
+            Console.Title = "ForEach";
+
+            string[] websites = new string[5] { "google", "youTube", "Facebook", "Baidu", "Yahoo" };
+
+            Console.WriteLine("Popular websites...");
+
+            int rank = 1;
+            foreach( string site in websites)
+            {
+                Console.WriteLine("Position: " + rank + "\t" + site);
+                rank++;
+            }
+
+            Console.ReadKey();
         }
     }
 }
