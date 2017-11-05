45f6f31fc75528c2d7149c050952cc8dae4289c4
Bob Cohen <bobcohen1969@yahoo.com>
2017-11-05 14:09:54 -0600

    While and Do While Loops, including break and continue

--- WhileLoop/Program.cs
+++ WhileLoop/Program.cs
@@ -10,6 +10,41 @@ namespace WhileLoop
     {
         static void Main(string[] args)
         {
+            Console.Title = "WhileLoop";
+
+            int[] nums = new int[10];
+            int i = 0;
+
+            //while loop
+            while (i < nums.Length)
+            {
+                nums[i] = i;
+                Console.Write(" | " + nums[i]);
+                i++;
+            }
+            Console.Write("\n\n");
+
+            //do while looop
+            do
+            {
+                i--;
+                //skip a single step
+                if (i == 8)
+                {
+                    Console.Write(" | Skipped");
+                    continue;
+                }
+                //Exite loop
+                if(i == 3)
+                {
+                    Console.Write(" | Done");
+                    break;
+                }
+               
+                Console.Write(" | " + nums[i]);
+            }
+            while (i > 0);
+            Console.ReadKey();
         }
     }
 }
