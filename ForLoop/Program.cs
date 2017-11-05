430e90a13058dca80f1a93b47a0396232ce8097c
Bob Cohen <bobcohen1969@yahoo.com>
2017-11-05 13:52:31 -0600

    For Loop, with nested loop

--- ForLoop/Program.cs
+++ ForLoop/Program.cs
@@ -10,6 +10,19 @@ namespace ForLoop
     {
         static void Main(string[] args)
         {
+            Console.Title = "ForLoop";
+
+            for(int i = 1; i < 4; i++)
+            {
+                Console.WriteLine("Loop iteration: " + i);
+
+                for(int j = 1; j < 4; j++)
+                {
+                    Console.WriteLine("The inner loop iteration: " + j);
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
