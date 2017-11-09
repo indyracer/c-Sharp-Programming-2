39e243ea80669a85904ea5b119183191d31995ca
Bob Cohen <bobcohen1969@yahoo.com>
2017-11-08 18:13:12 -0600

    Method Introduction Updated

--- Method/Program.cs
+++ Method/Program.cs
@@ -10,6 +10,36 @@ namespace Method
     {
         static void Main(string[] args)
         {
+            Console.Title = "Method";
+
+            bodyTemp();
+            Console.WriteLine("Fahrenheit:\t" + bodyTempF() + "°F");
+            Console.WriteLine("Kelvin:\t\t" + bodyTempK() + "K");
+            Console.ReadKey();
         }
+
+        static void bodyTemp()
+        {
+            Console.WriteLine("Body Temperature...");
+            Console.WriteLine("Centigrade:\t37°C");
+          }
+
+        static double bodyTempF()
+        {
+            double temperature = 98.6;
+            return temperature;
+        }
+
+        static int bodyTempK()
+        {
+            int temperature = 310;
+            return temperature;
+        }
+
+
+
+        }
+
+
     }
-}
+
