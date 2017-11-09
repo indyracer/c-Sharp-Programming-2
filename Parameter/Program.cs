1cf2b3356923a42de86e8dfeb08e645275ab25c4
Bob Cohen <bobcohen1969@yahoo.com>
2017-11-08 18:56:43 -0600

    Parameter examples

--- Parameter/Program.cs
+++ Parameter/Program.cs
@@ -10,6 +10,34 @@ namespace Parameter
     {
         static void Main(string[] args)
         {
+            Console.Title = "Parameter";
+
+            double weight;
+            string num;
+
+            num = getWeight(out weight);
+            Console.WriteLine(num + " lb = " + lbToKg(weight) + " kg");
+            kgToLb(ref weight);
+            Console.WriteLine(num + " kg = " + weight + " lb");
+            Console.ReadKey();
         }
+
+        static string getWeight(out double theWeight)
+        {
+            theWeight = 10;
+            return "Ten";
+        }
+
+        static double lbToKg (double pounds = 5)
+        {
+            return (pounds * 0.4539237);
+        }
+
+        static void kgToLb (ref double weight)
+        {
+            weight = weight / 0.45359237;
+        }
+
+
     }
 }
