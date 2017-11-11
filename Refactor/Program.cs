82caf77e2d49920c9bcdd8a4244b9187018caa75
Bob Cohen <bobcohen1969@yahoo.com>
2017-11-10 19:03:02 -0600

    Updated to add code

--- Refactor/Program.cs
+++ Refactor/Program.cs
@@ -10,6 +10,40 @@ namespace Refactor
     {
         static void Main(string[] args)
         {
+            Console.Title = "Refactor";
+
+            computeFactorials(1, 8);
+            Console.ReadKey();
+        }
+
+        /*static int factorial (int num)
+        {
+            int result;
+            if(num == 1)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = (factorial(num - 1) * num);
+            }
+            return result;
+        }
+        */
+
+        //refactored to optimize step 2
+        static int factorial (int num)
+        {
+            return (num == 1) ? 1 : (factorial(num - 1) * num);
+        }
+
+        static void computeFactorials (int num, int max)
+        {
+            while (num < max)
+            {
+                Console.WriteLine("Factorial of " + num + " : " + factorial(num));
+                num++;
+            }
         }
     }
 }
