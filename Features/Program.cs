7e0a1cf8b567b00cf4141eaa864c9e95b14c40ea
Bob Cohen <bobcohen1969@yahoo.com>
2017-11-12 09:05:15 -0600

    String Features

--- Features/Program.cs
+++ Features/Program.cs
@@ -10,6 +10,39 @@ namespace Features
     {
         static void Main(string[] args)
         {
+            Console.Title = "Features";
+
+            //get input from user
+            Console.Write("Please enter text: ");
+            string text = Console.ReadLine();
+
+            //test to see that user entered text before enter
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                Console.WriteLine("\nERROR:  No Text Found!");
+            }
+            else
+            {
+                //statements to report length
+                Console.WriteLine("\nThanks.  You entered:\n'" + text + "'");
+                Console.WriteLine("\nText Length: " + text.Length);
+
+                //statement to test how string begins
+                string query = text.StartsWith("C#") ? "Does" : "Does Not";
+                Console.WriteLine("Text " + query + " Starts with 'C#'");
+
+                //statement to test how string ends
+                query = text.EndsWith("steps") ? "Does" : "Does Not";
+                Console.WriteLine("Text " + query + " End with 'steps'");
+
+                //statement to test what string contains
+                query = text.Contains("easy") ? "Does" : "Does Not";
+                Console.WriteLine("Text " + query + " Contain 'easy'");
+            }
+
+            Console.ReadKey();
         }
+
+            
     }
 }
