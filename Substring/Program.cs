3ec9aa07bc63a7128f9c5008ad65baa754c437a0
Bob Cohen <bobcohen1969@yahoo.com>
2017-11-19 18:50:06 -0600

    Add project files.

---
+++ Substring/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
