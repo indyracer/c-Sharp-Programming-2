c92c64cfb7d11063bf738ad8a03b7e8aadcf118c
Bob Cohen <bobcohen1969@yahoo.com>
2017-12-03 17:20:08 -0600

    Reading text and lins

--- ReadText/Program.cs
+++ ReadText/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ReadText
 {
@@ -10,6 +11,43 @@ namespace ReadText
     {
         static void Main(string[] args)
         {
+            Console.Title = "ReadText";
+
+            //initialize variable
+            string path = "C:\\Users\\Bob\\Desktop\\word.txt";
+
+            //test if file already exists
+            if (File.Exists(path))
+            {
+                try
+                {
+                    //assign text file contents to a variable
+                    string text = File.ReadAllText(path);
+                    Console.WriteLine("File Read: " + path + "\n");
+                    Console.WriteLine(text + "\n");
+
+                    //assign text file contents to array variable and display each element with a counter
+                    string[] lines = File.ReadAllLines(path);
+                    int num = 1;
+                    foreach(string line in lines)
+                    {
+                        Console.WriteLine(num + " : " + line);
+                        num++;
+                    }
+
+                }
+                catch(Exception error)
+                {
+                    Console.WriteLine(error.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File Not Found: " + path);
+            }
+            Console.ReadKey();
+
+
         }
     }
 }
