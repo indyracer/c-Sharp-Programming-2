8c27669ec5fc634601b8a24ee99f0a2481553aa7
Bob Cohen <bobcohen1969@yahoo.com>
2017-12-23 16:49:29 -0600

    Catching Run-time errors

--- TryCatch/Program.cs
+++ TryCatch/Program.cs
@@ -10,6 +10,25 @@ namespace TryCatch
     {
         static void Main(string[] args)
         {
+            try
+
+    {
+                Console.Write("Please Enter a number: ");
+                double num1 = Convert.ToInt16(Console.ReadLine());
+
+                Console.Write("Now enter another number: ");
+                double num2 = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine("Total: " + (num1 + num2));
+            }
+
+    catch (OverflowException)
+            {
+
+                Console.WriteLine("\nMaximum: " + Int16.MaxValue);
+            }
+
+            Console.ReadKey();
         }
     }
 }
