f7418a5a6d2174f3ac6f1aef49ed6036c9c55707
Bob Cohen <bobcohen1969@yahoo.com>
2018-01-01 14:03:54 -0600

    Providing capability classes

--- Class Capability/Program.cs
+++ Class Capability/Program.cs
@@ -10,6 +10,59 @@ namespace Class_Capability
     {
         static void Main(string[] args)
         {
+            //create instances of non-base class objects
+            Pigeon joey = new Pigeon();
+            Chicken lola = new Chicken();
+            Caller call = new Caller();
+
+            call.describe(joey);
+            call.describe(lola);
+            Console.ReadKey();
+        }
+    }
+
+    //base class with 2 methods
+    public abstract class Bird
+    {
+        public abstract void talk();
+        public abstract void fly();
+    }
+
+    //derived class with 2 methods that over ride base class methods
+    public sealed class Pigeon : Bird
+    {
+        public override void talk()
+        {
+            Console.WriteLine("Pigeo Says: Coo! Coo!");
+        }
+
+        public override void fly()
+        {
+            Console.WriteLine("A Pigeon Flies Away...");
+        }
+    }
+
+    //derived class with 2 methods that over ride base class methods
+    public sealed class Chicken : Bird
+    {
+        public override void talk()
+        {
+            Console.WriteLine("Chicken Says:  Cluck! Cluck!");
+        }
+
+        public override void fly()
+        {
+            Console.WriteLine("I'm A Chicken - I Can't Fly!");
+        }
+    }
+
+    //class containing a single method that accepts object arguement
+    public sealed class Caller
+    {
+        public void describe(Bird obj)
+        {
+            obj.talk();
+            obj.fly();
         }
     }
 }
