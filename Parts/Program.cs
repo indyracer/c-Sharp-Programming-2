01580943229fa9e6bc70e81ea891c2cc90509cae
Bob Cohen <bobcohen1969@yahoo.com>
2018-01-01 14:16:20 -0600

    Employing Partial Classes

--- Parts/Program.cs
+++ Parts/Program.cs
@@ -10,6 +10,21 @@ namespace Parts
     {
         static void Main(string[] args)
         {
+            Console.Title = "Parts";
+
+            Sailboat boat = new Sailboat("Laser", "Classic");
+
+            boat.describe();
+            Console.ReadKey();
+            
+        }
+    }
+
+    public partial class Sailboat
+    {
+        public void describe()
+        {
+            Console.WriteLine("Sailboat: {0} {1}", make, model);
         }
     }
 }
