f5f1427bde4c39c9163c04ec3e4131f1058455ed
Bob Cohen <bobcohen1969@yahoo.com>
2018-01-01 10:51:09 -0600

    Inheriting class properties

--- Polygon/Program.cs
+++ Polygon/Program.cs
@@ -10,6 +10,48 @@ namespace Polygon
     {
         static void Main(string[] args)
         {
+            //create instance of Rectangle and Triangle
+            Rectangle rect = new Rectangle();
+            Triangle cone = new Triangle();
+
+            //set values via inherited setter method
+            rect.setValues(4, 5);
+            cone.setValues(4, 5);
+
+            //call area methods from derived class and display values
+            Console.WriteLine("Rectangle Area: " + rect.area());
+            Console.WriteLine("Triangle Area: " + cone.area());
+            Console.ReadKey();
+        }
+    }
+
+    //base class
+    public class Polygon
+    {
+        protected int width, height;
+
+        public void setValues(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    //defines class derived from base Polygon
+    public class Rectangle : Polygon
+    {
+        public int area()
+        {
+            return width * height;
+        }
+    }
+
+    //defines another class derived from base Polygon
+    public class Triangle : Polygon
+    {
+        public int area()
+        {
+            return (width * height) / 2;
         }
     }
 }
