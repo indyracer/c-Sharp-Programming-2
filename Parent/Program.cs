574be841529b79c5ccefc6eb0ae92d07a00a4e1c
Bob Cohen <bobcohen1969@yahoo.com>
2018-01-01 11:02:55 -0600

    Calling base constructors

--- Parent/Program.cs
+++ Parent/Program.cs
@@ -10,6 +10,51 @@ namespace Parent
     {
         static void Main(string[] args)
         {
+            //create instances of derived classes
+            Daughter anna = new Daughter();
+            Son brad = new Son();
+            Son carl = new Son(100);
+            Console.ReadKey();
+        }
+    }
+
+    //base class
+    public class Parent
+    {
+        //constructor
+        public Parent()
+        {
+            Console.WriteLine("Parent Called");
+        }
+
+        //overloaded constructor
+        public Parent(int num)
+        {
+            Console.WriteLine("Parent + Called: " + num);
+        }
+    }
+
+    //derived class Daughter
+    public class Daughter : Parent
+    {
+        public Daughter()
+        {
+            Console.WriteLine("\tDaughter Called\n");
+        }
+    }
+
+    //derived class Son
+    public class Son : Parent
+    {
+        public Son()
+        {
+            Console.WriteLine("\tSon Called\n");
+        }
+
+        //overloaded constructor
+        public Son(int num) : base(num)
+        {
+            Console.WriteLine("\tSon + Called: " + num);
         }
     }
 }
