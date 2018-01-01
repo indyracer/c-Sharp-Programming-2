69d520db1cc4e402277f19b280765427363accfc
Bob Cohen <bobcohen1969@yahoo.com>
2018-01-01 11:18:08 -0600

    Hiding base methods

--- Man/Program.cs
+++ Man/Program.cs
@@ -10,6 +10,49 @@ namespace Man
     {
         static void Main(string[] args)
         {
+            //instance of base class
+            Man henry = new Man();
+
+            //instance of derived class
+            Hombre enrique = new Hombre();
+
+            //call  simple method inherited by instance from base class
+            henry.speak();
+
+            //call to overloaded method inherited from base class
+            henry.speak("It's a beautiful evening");
+
+            //call to hiding method in derived class
+            enrique.speak("Hola...");
+
+            //explicit cast to explicitly call overloaded method in base class
+            ((Man)enrique).speak("Es una tarde hermosa");
+
+            Console.ReadKey();
+        }
+    }
+
+    //base class
+    public class Man
+    {
+        public void speak()
+        {
+            Console.WriteLine("Hello: ");
+        }
+
+        public void speak (string message)
+        {
+            Console.Write(message + "!\n");
+        }
+    }
+
+    //derived class, method intentionally hides overloaded method from base class
+    public class Hombre : Man
+    {
+        public new void speak(string message)
+        {
+            base.speak();
+            Console.WriteLine(message);
         }
     }
 }
