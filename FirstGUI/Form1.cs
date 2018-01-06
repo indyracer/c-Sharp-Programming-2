32dd4d57ae015ab3b389bd6b71d7f74c959bd176
Bob Cohen <bobcohen1969@yahoo.com>
2018-01-06 12:01:13 -0600

    First GUI, Starting a Forms Project, Writing functional code, adding visual controls

--- FirstGUI/Form1.cs
+++ FirstGUI/Form1.cs
@@ -16,5 +16,25 @@ namespace FirstGUI
         {
             InitializeComponent();
         }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Fuchsia;
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            button1.BackColor = Color.Lime;
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            button1.BackColor = Color.Aqua;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("C# Programming in easy steps");
+        }
     }
 }
