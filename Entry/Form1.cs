be833bdb04537643c4d79d0c864111a51e0ca656
Bob Cohen <bobcohen1969@yahoo.com>
2018-01-13 09:14:48 -0600

    Gathering text entries

--- Entry/Form1.cs
+++ Entry/Form1.cs
@@ -12,9 +12,44 @@ namespace Entry
 {
     public partial class Form1 : Form
     {
+        private int num;
+        public void setNum(int num)
+        {
+            this.num = num;
+        }
+        public int getNum()
+        {
+            return num;
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Random rnd = new Random();
+            setNum(rnd.Next(1, 21));
         }
+
+        public void rate(int guess, int num)
+        {
+            if(guess < num)
+            {
+                label2.Text = "Too Low!";
+            }
+            else if (guess > num)
+            {
+                label2.Text = "Too High!";
+            }
+            else
+            {
+                label2.Text = "*** Correct ***";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int guess = Convert.ToInt16(textBox1.Text);
+            rate(guess, getNum());
+        }
+
     }
 }
