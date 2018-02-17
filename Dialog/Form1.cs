3335386b7feb956ed62a9b8342dc9cc87f13892d
Bob Cohen <bobcohen1969@yahoo.com>
2018-02-16 19:29:40 -0600

    updated

--- Dialog/Form1.cs
+++ Dialog/Form1.cs
@@ -16,5 +16,31 @@ namespace Dialog
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK) this.BackColor = colorDialog1.Color;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK) textBox1.Font = fontDialog1.Font;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictureBox1.Image = new Bitmap(openFileDialog1.FileName);
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show("Error Select An Image File!");
+                }
+            }
+        }
     }
 }
